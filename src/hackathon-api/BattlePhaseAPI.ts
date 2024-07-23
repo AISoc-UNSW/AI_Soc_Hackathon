d9c8f28579cec60314c3218223769f5a85f08a26
ceeeen <raymondcn2002@gmail.com>
2024-07-23 17:22:54 +1000

    add battle state

--- src/hackathon-api/BattlePhaseAPI.ts
+++ src/hackathon-api/BattlePhaseAPI.ts
@@ -1,6 +1,8 @@
 import BattleScene from "#app/battle-scene.js";
+import Battle from "#app/battle.js";
 import { PokeballType } from "#app/data/pokeball.js";
 import { PlayerPokemon } from "#app/field/pokemon.js";
+import { battle } from "#app/locales/de/battle.js";
 import { CommandPhase } from "#app/phases.js";
 import { Command } from "#app/ui/command-ui-handler.js";
 import { Mode } from "#app/ui/ui.js";
@@ -28,26 +30,6 @@ export const BallCommand = (scene: BattleScene) => {
 };
 
 /**
- * Retrieves the player's party of Pokémon.
- *
- * @param scene - The current BattleScene instance.
- * @returns {PlayerPokemon[]} - An array of PlayerPokemon objects representing the player's party.
- */
-export const GetParty = (scene: BattleScene): PlayerPokemon[] => {
-  return scene.getParty();
-};
-
-/**
- * Retrieves the names of the Pokémon in the player's party.
- *
- * @param scene - The current BattleScene instance.
- * @returns {string[]} - An array of strings representing the names of the Pokémon in the player's party.
- */
-export const GetPartyNames = (scene: BattleScene): string[] => {
-  return GetParty(scene).map((p) => p.name);
-};
-
-/**
  * Issues a command to switch the active Pokémon.
  *
  * This function handles the command to switch the active Pokémon during battle.
@@ -58,9 +40,52 @@ export const GetPartyNames = (scene: BattleScene): string[] => {
  * @param index - The index of the Pokemon to switch to in the party. Index is of the array returned by GetParty or GetPartyNames
  */
 export const SwitchCommand = (scene: BattleScene, index: number) => {
-  const commandPhase = scene.getCurrentPhase();
+    const commandPhase = scene.getCurrentPhase();
+    if (commandPhase instanceof CommandPhase) {
+        commandPhase.handleCommand(Command.POKEMON, index, false);
+    }
+};
 
-  if (commandPhase instanceof CommandPhase) {
-    commandPhase.handleCommand(Command.POKEMON, index, false);
-  }
+/**
+ * Retrieves the player's party of Pokémon.
+ *
+ * @param scene - The current BattleScene instance.
+ * @returns {PlayerPokemon[]} - An array of PlayerPokemon objects representing the player's party.
+ */
+export const GetParty = (scene: BattleScene): PlayerPokemon[] => {
+    return scene.getParty();
+};
+
+/**
+ * Retrieves the names of the Pokémon in the player's party.
+ *
+ * @param scene - The current BattleScene instance.
+ * @returns {string[]} - An array of strings representing the names of the Pokémon in the player's party.
+ */
+export const GetPartyNames = (scene: BattleScene): string[] => {
+    return GetParty(scene).map((p) => p.name);
+};
+
+export const getBattleState = (scene: BattleScene) => {
+    const partyStats = scene.getParty().map((pokemon) => {
+        return {
+            name: pokemon.name,
+            hp: pokemon.hp,
+            species: pokemon.species,
+            moveset: pokemon.moveset,
+        };
+    });
+    const enemyStats = scene.currentBattle.enemyParty.map((pokemon) => {
+        return {
+            name: pokemon.name,
+            hp: pokemon.hp,
+            species: pokemon.species,
+            moveset: pokemon.moveset,
+        };
+    });
+    const battleState = {
+        playerParty: partyStats,
+        enemyParty: enemyStats,
+    };
+    console.log(battleState);
 };
